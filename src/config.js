442ad6a39907ff8d74bb1100ca33e361c09c4404
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2017-10-31 22:28:14 +0100

    add config object

--- src/config.js
+++ src/config.js
@@ -1,3 +1,18 @@
 module.exports = {
-    telegramApiKey: '439144820:AAHcdzVd42HaFjH_Ql7v39qKdWa4GQbDZ9g'
+    api: {
+        telegram: {
+            key: '439144820:AAHcdzVd42HaFjH_Ql7v39qKdWa4GQbDZ9g'
+        },
+        discord: {
+            webhook: 'https://discordapp.com/api/webhooks/375027503028502529/ZMmMaUOMbZBTdBLILUXgoC6QseT-s3DPS8xRBN3lmrt95Qenl7v7zZw9-pu-Ht_sbbHa'
+        }
+    },
+    mongodb: {
+        host: '127.0.0.1',
+        port: 27017,
+        path: 'mmp2'
+    },
+    server: {
+        port: 8080
+    }
 }
