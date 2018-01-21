ac90edad44e02bce857b3c5fbe6e1bcee589b2eb
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-01-21 22:12:06 +0100

    fix(api): update router

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :tasks
   resources :groups
-  resources :users
+  resources :tasks
+
+  resources :groups do
+    resources :users
+    resources :tasks
+  end
+  resources :users do
+    resources :groups
+    resources :tasks
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
