33aee132a2f5db06cf41dd412f46c53d2a80fa2d
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-01-28 22:58:18 +0100

    fix(api): allow requests from every origin

--- api/config/application.rb
+++ api/config/application.rb
@@ -14,7 +14,8 @@ module Api
     # Cors erlauben
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:8080'
+        #origins 'http://localhost:8080'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
       end
     end
