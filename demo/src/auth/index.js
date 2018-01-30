33ee4e333c72490678184fd5f967fdd20011903e
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-01-30 15:09:56 +0100

    feat(auth): use json token for requests, add request wrapperm add some helper methods to auth

--- demo/src/auth/index.js
+++ demo/src/auth/index.js
@@ -1,34 +1,53 @@
 import notification from '@/lib/notification'
+import request from '@/lib/request'
 
-let email = null;
-let password = null;
+let user = null;
+let auth_token = null;
+
+const local_token = sessionStorage.getItem('auth_token')
+const local_user = sessionStorage.getItem('auth_token')
+
+if(local_token) {
+    auth_token = local_token
+    user = local_user
+}
 
 export default {
     isSignedIn() {
-        return email !== null
+        return auth_token !== null
+    },
+    getUser() {
+        return user
+    },
+    getUID() {
+        if(user) return user.id
+        return null
+    },
+    getAuthToken() {
+        return auth_token
+    },
+    signOut() {
+        user = null
+        auth_token = null
     },
     signIn(email, password) {
-        const formData = new FormData();
-        formData.append('email', email);
-        formData.append('password', password);
-
-        fetch('http://localhost:3000/login', {
-            mode: 'cors',
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-            },
-        })
-        .then(response => response.json())
-        .then(status => {
-            if(status.error !== undefined && status.error === true) {
+        return new Promise((resolve, reject) => {
+            request.fetch(`http://localhost:3000/login`, 'POST', {
+                email,
+                password
+            })
+            .then(res => {
+                notification.success(res);
+                user = res.user
+                auth_token = res.auth_token
+                sessionStorage.setItem('auth_token', auth_token)
+                sessionStorage.setItem('user', user)
+                resolve(user)
+            })
+            .catch(err => {
                 notification.error(status.message);
-                return false;
-            }
-            else {
-                notification.success(status.message);
-                return true;
-            }
+                reject(status.message);
+            })
         })
     }
 }
