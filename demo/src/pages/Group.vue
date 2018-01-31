13261e0363002ef410cfb5f9755e843f030ca38e
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2018-01-31 12:18:53 +0100

    feat(group): add styling for members and switch buttons

--- demo/src/pages/Group.vue
+++ demo/src/pages/Group.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
     <h2 class="text--center text--left">Mitglieder</h2>
-    <ul class="members">
-      <li v-for="member in group_members" class="list__item__wrapper" :data-id="member.id">
-        <div class="list__item fade-out">{{ member.name }}</div>
-      </li>
-    </ul>
+    <div class="members">
+      <div class="members__item members__item__add"><span class="icon-plus"></span></div>
+      <div v-for="(item, index) in group_members" class="members__item" :data-id="item.id">
+        <!-- add placeholder image if there is no avater set for the user -->
+        <div v-if="item.avatar == null" class="members__item__content"></div>
+        <img v-else src="">
+
+        <div class="members__item__name">{{ item.name }}</div>
+      </div>
+    </div>
     <button class="btn btn--red btn--left" v-on:click="leaveGroup">Gruppe verlassen</button>
+
+    <span class="horizontal-line"></span>
+
     <h2 class="text--center text--left">Benachrichtigungen</h2>
-    <p>Push</p>
-    <p>E-Mail</p>
+
+    <div class="switch-wrapper">
+      <div class="switch">
+          <input type="checkbox" name="switch" class="switch__checkbox" id="push">
+          <label class="switch__label" for="push"></label>
+      </div>
+      <p>Push</p>
+    </div>
+
+    <div class="switch-wrapper">
+      <div class="switch">
+          <input type="checkbox" name="switch" class="switch__checkbox" id="email">
+          <label class="switch__label" for="email"></label>
+      </div>
+      <p>E-Mail</p>
+    </div>
+
   </div>
 </template>
 
@@ -27,7 +50,9 @@
     },
     methods: {
       leaveGroup: function() {
-        request.fetch(`http://localhost:3000/groups/${this.$route.params.id}/users/${auth.getUID()}.json`, method = 'DELETE', params = {})
+        console.log("exec leaveGroup");
+        console.log(`http://localhost:3000/groups/${this.$route.params.id}/users/${auth.getUID()}.json`);
+        request.fetch(`http://localhost:3000/groups/${this.$route.params.id}/users/${auth.getUID()}.json`, 'DELETE', params = {});
       }
     },
     created() {
@@ -47,9 +72,53 @@
 <style lang="scss">
 
 .members {
-  width: 100%;
-  float: left;
-  padding: 0;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
   margin-top: 1.3em;
+
+  .members__item {
+    width: 30%;
+    margin-bottom: 1.3em;
+
+    &:last-child {
+      margin-right: 0;
+    }
+
+    .members__item__content {
+      width: 90px;
+      height: 90px;
+      background-color: $grey;
+      border-radius: 1em;
+    }
+
+    .members__item__name {
+      font-weight: 400;
+      color: $black;
+      margin-top: 0.3em;
+    }
+  }
+
+  .members__item__add {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 90px;
+    height: 90px;
+    background-color: inherit;
+    color: $blue;
+    border: 3px solid rgba($blue, 0.4);
+    border-radius: 1em;
+    margin-bottom: 2.4em;
+
+    span {
+      font-size: 3.3em;
+      color: rgba($blue, 0.4);
+    }
+  }
+}
+
+button.btn {
+  margin-top: 0.5em;
 }
 </style>
