a7bef4177611716efc28a6e3c42aa3b0c7ec0fa8
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-01-31 17:09:27 +0100

    fix(pages): fix task list

--- demo/src/components/Task.vue
+++ demo/src/components/Task.vue
@@ -1,14 +1,15 @@
 <template>
 <li v-bind:class="{ 'task--done': task.status }" class="task">
-  <h4>{{ task.name }}</h4>
-  <p class="task__description">
+  <div class="task__info">
+    <input type="checkbox" v-model="task.status" class="task__done">
+    <div class="task__title">
+      {{ task.name }}</h4>
+    </div>
+  </div>
+  <!--<div class="task__description">
     {{ task.description }}
-  </p>
-
-  <label>
-      Erledigt: <input type="checkbox" v-model="task.status">
-      </label>
-  <span v-if="task.duedate">{{ task.duedate }}</span>
+  </div>-->
+  <span v-if="task.due_date" class="task__duedate">{{ renderDate(task.due_date) }}</span>
 </li>
 </template>
 
@@ -16,21 +17,35 @@
 import request from '@/lib/request'
 import auth from '@/auth'
 
+const getMonthName = (idx) => {
+  const months = ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez']
+  return months[idx]
+}
+
 export default {
   name: 'task-component',
   props: ['task'],
+  methods: {
+    renderDate: (date) => {
+      const d = new Date(date);
+      const mo = getMonthName(d.getMonth());
+
+      if(d.getUTCHours() == 0 && d.getUTCMinutes() == 0) return `${d.getDay()}. ${mo}`
+      else return `${d.getDay()}. ${mo} / ${d.getHours()}:${d.getMinutes()}`
+    }
+  },
   watch: {
     task: {
       handler: function(newValue) {
         request.fetch(
-          `http://localhost:3000/users/${auth.getUID()}/tasks/${this.task.id}.json`,
-          'PUT', {
-            'task[done]': this.task.status ? 1 : 0
-          }
-        )
-        .then(data => {
-          console.log(data);
-        });
+            `http://localhost:3000/users/${auth.getUID()}/tasks/${this.task.id}.json`,
+            'PUT', {
+              'task[done]': this.task.status ? 1 : 0
+            }
+          )
+          .then(data => {
+            console.log(data);
+          });
       },
       deep: true
     }
@@ -42,7 +57,22 @@ export default {
 <style lang="scss">
 .task {
     margin-bottom: 2em;
+    display: flex;
+    justify-content: space-between;
 
+    .task__info {
+      display: flex;
+      font-weight: 500;
+    }
+
+    .task__done {
+        margin-right: .5em;
+    }
+
+    .task__duedate {
+      color: $grey-dark;
+      font-size: 0.8em;
+    }
     h4 {
         margin: 0 0 0.5em;
     }
