b3c4b986a321c05cd83d67ccd93a67a086fa394d
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-01-31 12:35:45 +0100

    fix(api): fix json response delete user from group

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -108,7 +108,7 @@ class UsersController < ApplicationController
         group.users.delete(@user)
         respond_to do |format|
           format.html { redirect_to groups_url, notice: 'User was successfully remove from Group.' }
-          format.json { head :no_content }
+          format.json { render json: {message: 'ok'}, status :ok }
         end
       else
         respond_to do |format|
