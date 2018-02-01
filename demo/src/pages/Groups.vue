ff55a0b9653d83001abe4a576eb91807712703ec
Moritz Lang <mlang.mmt-b2016@fh-salzburg.ac.at>
2018-02-01 16:41:32 +0100

    feat(invite): add join functionality for invite code

--- demo/src/pages/Groups.vue
+++ demo/src/pages/Groups.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h2 class="text--center text--left">Deine Gruppen</h2>
+    <input id="invite-code" type="text" placeholder="Füge hier einen Beitrittscode ein">
+    <button class="btn btn--default" v-on:click="joinGroup('invite-code')">Beitreten</button>
     <ul class="groups">
       <li v-for="group in groups" class="list__item__wrapper" :data-id="group.id">
         <router-link :to="{ name: 'group_id', params: { id: group.id }}">
@@ -15,6 +17,7 @@
 <script>
   import auth from '@/auth'
   import request from '@/lib/request'
+  import notification from '@/lib/notification'
 
   export default {
     name: 'groups-page',
@@ -24,14 +27,59 @@
         user: {}
       }
     },
+    methods: {
+      joinGroup: function(id) {
+        request.fetch(`http://localhost:3000/invitecodes.json`)
+          .then(codes => {
+            let codeInput = document.getElementById(id).value;
+            let inviteCode = codes.find(elem => elem.code === codeInput)
+
+            // input matches code in database
+            if(inviteCode !== undefined) {
+              if(isUserInGroup(inviteCode.group_id, this.groups)) {
+                notification.error("Du bist bereits in der Gruppe");
+              }
+              else {
+                // join the group
+                request.fetch(`http://localhost:3000/groups/${inviteCode.group_id}/users.json`, 'POST', { id: auth.getUID() })
+                       .then(() => // reload data
+                         request.fetch(`http://localhost:3000/users/${auth.getUID()}.json`)
+                           .then(user => {
+                             this.user = user;
+                             this.groups = user.groups
+                         })
+                      );
+
+                // go to the group page
+                this.$router.push({ path: `groups/${inviteCode.group_id}`})
+
+                notification.success("Gruppe erfolgreich beigetreten");
+              }
+            }
+            else {
+              notification.error("Falscher Beitrittscode");
+            }
+          });
+      }
+    },
     created() {
       request.fetch(`http://localhost:3000/users/${auth.getUID()}.json`)
-        .then(user => {
-          this.user = user;
-          this.groups = user.groups
-        });
+             .then(user => {
+               this.user = user;
+               this.groups = user.groups
+             });
     }
   }
+
+  function isUserInGroup(group_id, groups) {
+    if(groups.find(group => group.id === group_id)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
 </script>
 
 <style lang="scss">
