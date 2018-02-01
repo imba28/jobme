d29f4123a32de8adf53036fcf10c584a169ac022
Jonas Bücker <jbuecker.incoming@fh-salzburg.ac.at>
2018-02-01 17:38:03 +0100

    merge

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -20,20 +20,20 @@ ActiveRecord::Schema.define(version: 20180201120539) do
     t.index ["admin_id"], name: "index_groups_on_admin_id"
   end
 
-  create_table "memberships", force: :cascade do |t|
-    t.integer "group_id"
-    t.integer "user_id"
-    t.integer "membership_type", default: 0, null: false
-    t.index ["group_id"], name: "index_memberships_on_group_id"
-    t.index ["user_id"], name: "index_memberships_on_user_id"
-  end
-
   create_table "invitecodes", force: :cascade do |t|
     t.string "code"
     t.integer "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_invitecodes_on_group_id"
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "user_id"
+    t.integer "membership_type", default: 0, null: false
+    t.index ["group_id"], name: "index_memberships_on_group_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
