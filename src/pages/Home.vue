22cefd41d93e96b84b5527a1032888398624958f
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-04-06 15:57:13 +0200

    feat: add login form on home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,100 +1,99 @@
 <template>
-  <div>
-    <div class="welcome--text__container">
-      <h1>Hallo Ich bin Tom!</h1>
-      <p>Wie ich sehe, bist du neu hier.<br>
-      Erstelle ein Konto, um  den <br> task-o-mat nutzen zu können.
-      </p>
-      <button class="btn btn--default" >Jetzt registrieren</button>
+  <div class="welcome">
+    <div class="container">
+      <div class="image">
+        <img src="http://via.placeholder.com/400x400">
+      </div>
+      <div class="button-group">
+        <button class="btn btn--secondary btn--default--focus">Sign up</button>
+        <button class="btn btn--default">Sign in</button>
+      </div>
     </div>
-    <div class="arrow-down"></div>
-   <div class="tom__container">
-      <img src="../assets/tom-full-body.svg">
-   </div>
-   <div class="login--info__container">
-      <p>Du hast schon ein Konto?</p>
-      <a href="#/login" class="link--more">Hier geht's zum Login</a>
-   </div>
+    <div class="login--info__container">
+      <div class="form__container">
+        <form v-on:submit.prevent="signIn">
+          <div class="input__container">
+            <input ref="user" id="email" class="input" type="text" required="true" placeholder="What's your name?" />
+            <label class="input__label" id="email__label" for="email">
+              <span class="icon-user"></span>
+            </label>
+          </div>
+          <div class="text-center pt-1">
+            <div>
+              <button type="submit" class="btn btn--default display--center display--block" style="width: 100%; box-sizing: border-box;">Login</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
-  </template>
+</template>
 
 <script>
-
-
+  import auth from '@/auth'
+  import router from '@/router'
   export default {
     name: 'welcome-page',
-    data(){
+    data() {
       return {
         page: "Welcome",
       }
+    },
+    methods: {
+      signIn() {
+        const email = this.$refs['user'].value
 
+        auth.signIn(email, null)
+          .then((user) => {
+            router.push({
+              path: '/explore-childhood'
+            })
+          })
+      }
     }
   }
+
 </script>
 
 <style lang="scss">
-  .view{
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    height:100%;
+  .welcome {
+    .input__container {
+      margin-top: 1em;
+    }
+    .container {
+
+      .button-group {
+        display: flex;
+
+        .btn {
+          width: 50%;
+
+          &:last-child {
+            border-left-width: 0;
+          }
+        }
+      }
+
+      .image img {
+        width: 100%;
+        max-width: 400px;
+        margin: 0 auto;
+        background: #ddd;
+      }
+
+      h1 {
+        margin-bottom: 0.3em;
+      }
+
+      p {
+        padding: 0 2em 0 2em;
+      }
+
+      button {
+        align-self: center;
+        margin: 0.5em 0 0.5em 0em;
+      }
+    }
   }
-
-  .welcome--text__container{
-    margin-top: 1.5em;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    background-color: $grey;
-    padding: 0.8em;
-    border-radius: 5%;
-    align-self: center;
-
-  h1{
-    margin-bottom: 0.3em;
-  }
-
-  p{
-    padding: 0 2em 0 2em;
-  }
-
-  button{
-    align-self: center;
-    margin: 0.5em 0 0.5em 0em;
-  }
-}
-
-  .tom__container{
-    height: 10em;
-    width: 10em;
-    margin-bottom: 5em;
-    align-self: center;
-  }
-
-  .login--info__container{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    align-self: center;
-  }
-
-  p{
-    margin: 0 0 0.3em 0;
-  }
-
-  a{
-    text-decoration: underline;
-  }
-
-  .arrow-down {
-    align-self: center;
-    width: 0;
-    height: 0;
-    border-left: 20px solid transparent;
-    border-right: 20px solid transparent;
-    border-top: 20px solid $grey;
-    margin-bottom: 1em;
-  }
-
 
 </style>
