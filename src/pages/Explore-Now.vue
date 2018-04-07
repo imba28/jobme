61f64686a7d5e1ec82087e2a76e43b76ad161d36
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2018-04-07 14:06:04 +0200

    update titles

--- src/pages/Explore-Now.vue
+++ src/pages/Explore-Now.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="explore-now">
     <div>
-      <h1>Nowadays</h1>
+      <h1>What do you love to do now?</h1>
       <ul class="icons">
         <li v-for="hobby in hobbies">
           <div class="inner">
