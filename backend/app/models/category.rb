880430f1297f813819e758abf75a20d95d19be5d
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-04-23 14:38:42 +0200

    feat(upload): add image upload
    upload category icons
    
    #14

--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -1,3 +1,13 @@
 class Category < ApplicationRecord
     has_many :subcategories
+
+    mount_uploader :icon_url, ImageIconUploader
+
+    validates_processing_of :icon_url
+    validate :image_size_validation
+
+    private 
+        def image_size_validation
+                errors[:icon_url] << "should be less than 500KB" if icon_url.size > 0.5.megabytes 
+        end
 end
