eee201c9e321442b43cf7cbf4b86fa5fb659ed27
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2018-04-23 11:13:21 +0200

    chore(backend): change dev env to sqlite3
    #11

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180423083250) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "icon_url"
@@ -33,7 +30,7 @@ ActiveRecord::Schema.define(version: 20180423083250) do
   create_table "subcategories", force: :cascade do |t|
     t.string "name"
     t.text "icon_url"
-    t.bigint "category_id"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_subcategories_on_category_id"
@@ -43,12 +40,10 @@ ActiveRecord::Schema.define(version: 20180423083250) do
     t.string "name"
     t.string "email"
     t.text "pwd"
-    t.bigint "job_id"
+    t.integer "job_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["job_id"], name: "index_users_on_job_id"
   end
 
-  add_foreign_key "subcategories", "categories"
-  add_foreign_key "users", "jobs"
 end
