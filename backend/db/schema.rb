3941ea300bdff0a64984864246c0dfc7aa119fd0
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-04-23 13:24:42 +0200

    feat(login): add login with github, add nice header
    #13

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423093422) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+ActiveRecord::Schema.define(version: 20180423105841) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -23,8 +20,8 @@ ActiveRecord::Schema.define(version: 20180423093422) do
   end
 
   create_table "job_subcategory_joins", force: :cascade do |t|
-    t.bigint "job_id"
-    t.bigint "subcategory_id"
+    t.integer "job_id"
+    t.integer "subcategory_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["job_id"], name: "index_job_subcategory_joins_on_job_id"
@@ -55,11 +52,10 @@ ActiveRecord::Schema.define(version: 20180423093422) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "provider"
+    t.string "uid"
+    t.string "avatar_url"
     t.index ["job_id"], name: "index_users_on_job_id"
   end
 
-  add_foreign_key "job_subcategory_joins", "jobs"
-  add_foreign_key "job_subcategory_joins", "subcategories"
-  add_foreign_key "subcategories", "categories"
-  add_foreign_key "users", "jobs"
 end
