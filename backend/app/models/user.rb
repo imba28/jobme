5ccdfaa7027be1944be94c2ff721de488d2522c8
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2018-04-24 14:15:52 +0200

    fix(backend): workaround: disable confirmation of password (oauth)

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_confirmation_of :password, :on => :create
-  validates_uniqueness_of :name, :email, :on => :create
-
+  # validates_confirmation_of :password, :on => :create
+  validates_uniqueness_of :name, :email, :on => :create  
+  
   def self.find_or_create_with_omniauth auth
     user = User.find_or_create_by!(
         provider: auth['provider'],
         uid: auth['uid'],
-        password: SecureRandom.urlsafe_base64
 
     )
     user.name = auth.info.nickname
