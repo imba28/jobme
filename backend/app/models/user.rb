6464de812ffdbad149d812efe552eb17a66e5b12
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2018-04-24 14:26:05 +0200

    fix(backend): workaround try to disable password check (oauth)

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
-  has_secure_password
+  # has_secure_password
   # validates_confirmation_of :password, :on => :create
   validates_uniqueness_of :name, :email, :on => :create  
   
   def self.find_or_create_with_omniauth auth
     user = User.find_or_create_by!(
         provider: auth['provider'],
-        uid: auth['uid'],
-
+        uid: auth['uid']
     )
-    user.name = auth.info.nickname
+    user.name = auth.info.name
+    user.email = auth.info.email
     user.avatar_url = auth.info.image
 
     user.save
