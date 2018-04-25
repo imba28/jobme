10689c409bb3b3343a760311eef5291a63cbe912
Lukas Kern <lkern.mmt-b2016@fh-salzburg.ac.at>
2018-04-25 14:45:34 +0200

    feat: matching jobs

--- backend/app/uploaders/image_icon_uploader.rb
+++ backend/app/uploaders/image_icon_uploader.rb
@@ -39,7 +39,7 @@ class ImageIconUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-    %w(jpg jpeg gif png)
+    %w(jpg jpeg gif png svg)
   end
 
   # Override the filename of the uploaded files:
