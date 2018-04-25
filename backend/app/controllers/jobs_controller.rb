8bf8b286340d5b25101176ddbd82e4c79455d104
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2018-04-25 10:11:24 +0200

    fix(authentication): Sorry hab dem Admin zu wenig Rechte gegeben

--- backend/app/controllers/jobs_controller.rb
+++ backend/app/controllers/jobs_controller.rb
@@ -1,7 +1,7 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  before_action :is_admin?!, only: [:new, :edit, :create, :update, :destroy]
+  before_action :is_admin?, only: [:new, :edit, :create, :update, :destroy]
   before_action :check_format, only: [:index, :show]
 
   # GET /jobs
