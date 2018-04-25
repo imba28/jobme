cc9229d3d0da04503914acf50af704e7eecbae7a
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2018-04-25 09:27:54 +0200

    store oauth uid in session

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  # before_action :authenticate_user!
+  before_action :authenticate_admin!, only: [:edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
