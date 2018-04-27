c84b06b461428c4e1f6e356ee53b8803fc3b8208
Lukas Kern <lkern.mmt-b2016@fh-salzburg.ac.at>
2018-04-27 09:22:36 +0200

    feat: fix bu

--- backend/app/controllers/categories_controller.rb
+++ backend/app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :is_admin?, only: [:new, :edit, :create, :update, :destroy]
   before_action :check_format, only: [:index, :show]
 
