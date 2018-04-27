6f7018818c0d9a6b33b6c04ff0ffa6b20434da69
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2018-04-27 09:32:59 +0200

    feat(backend): validation of register

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   mount_uploader :avatar_url, ImageIconUploader
   validates_confirmation_of :password, :on => :create
   validates_uniqueness_of :name, :email, :on => :create  
-  validates :name, :email, presence: {message : "Required!"}
+  validates :name, :email, presence: {message: "can't be blank!"}
   
   def self.find_or_create_with_omniauth auth
     if !User.find_by(provider: auth['provider'],
