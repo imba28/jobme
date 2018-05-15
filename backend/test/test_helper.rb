0e4e117e58c1fd3f92e2497f2b2be4089d57c2e7
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2018-05-15 06:02:17 +0200

    refactor(backend): refactor code according to robocop, disable some rules because they don't make se

--- backend/test/test_helper.rb
+++ backend/test/test_helper.rb
@@ -4,7 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 
-class ActiveSupport::TestCase
+class ActiveSupport < TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
