1524f2b9286a4eb5aec98bfe2c0d41325b9fd749
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2018-05-15 06:32:58 +0200

    refactor(backend): rails_best_practices
    remove empty helper methods + change class variables to local variables + law of demeter

--- backend/app/models/subcategory.rb
+++ backend/app/models/subcategory.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 class Subcategory < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :jobs
+  delegate :name, :to => :category, :prefix => true
 
   mount_uploader :icon, ImageIconUploader
 
